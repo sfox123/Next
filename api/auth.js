8528920d4caf193c47c7cdcd6813745afec182c0
sfox123 <shagyfox133@gmail.com>
2021-05-26 10:11:06 +0530

    Second https

--- api/auth.js
+++ api/auth.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://us-central1-express-439e0.cloudfunctions.net/app",
+  baseURL: "https://us-central1-express-439e0.cloudfunctions.net/app",
 });
